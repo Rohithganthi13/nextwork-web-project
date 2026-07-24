5608a19025e2c10d55eed4cdd35a237139676030
Rohith-VoltusWave <rohith.ganthi@voltuswave.com>
2026-07-24 10:34:01 +0530

    updated the port to 80

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -10,4 +10,4 @@ IMAGE_REPO_NAME=web-app
 
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 docker pull $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$IMAGE_REPO_NAME:latest
-docker run -d --name web-app -p 8081:8080 $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$IMAGE_REPO_NAME:latest
+docker run -d -p 80:8080 047719635525.dkr.ecr.ap-south-1.amazonaws.com/web-app:latest
